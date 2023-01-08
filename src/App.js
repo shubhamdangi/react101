60ef8d9061c040872bf639d24ee5d01cf96eeea2
shubhamdangi <shubham.d3v@gmail.com>
2023-01-08 18:50:23 +0530

    ci/cd test

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello React CI/CD</h1>
+      <h1>Hello React CI/CD.</h1>
       {/* <NotesState>
         <Parent />
         <Display />
